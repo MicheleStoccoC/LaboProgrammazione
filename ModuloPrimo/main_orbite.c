ca7849949d2ad864af4ebf7316c28bea0df47ab5
MicheleStoccoC <official.michelesc@gmail.com>
2025-10-30 14:38:56 +0100

    Caricamento cartella ModuloPrimo con main

---
+++ ModuloPrimo/main_orbite.c
@@ -0,0 +1,23 @@
+#include"stdio.h"
+#include"stdlib.h"
+#include"math.h"
+#include"string.h"
+#include "stdarg.h"
+
+    /* COSTANTI MATEMATICHE */
+#define PI 3.14159
+#define E 2.71828
+    /* COSTANTI ASTROFISICHE */
+#define GNEWT strtod(6.67e-11)
+#define MSUN strtod(2.0e33)
+#define VTER 100.0
+#define UA strtod(1.5e8)
+
+    /* FUNZIONI */
+
+
+
+    /* MAIN */
+int main(){
+
+}
